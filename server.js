0a81ed649627e9dc2cd1d7eb89def8ce7998802d
WolfSoftwareDev <43505755+WolfSoftwareDev@users.noreply.github.com>
2019-08-10 03:16:39 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
-var request = require('request');
-module.exports.Votecount = (id, botid, token) => {
-    request('https://wolfbotslist/api/getvotes/' + id + '/' + botid + '/' + token, function (error, response, body) {
-        return(body)
+const https = require('https');
+
+async function Votecount(id, botid, token){
+    var enddata;
+    https.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', (resp) => {
+    let data = '';
+
+    // A chunk of data has been recieved.
+    resp.on('data', (chunk) => {
+        data += chunk;
     });
+
+    // The whole response has been received. Print out the result.
+    resp.on('end', () => {
+        enddata = data
+        console.log(JSON.parse(data));
+    });
+
+    }).on("error", (err) => {
+    console.log("Error: " + err.message);
+    });
+    return(enddata)
+}
+
+module.exports.Votecount = async (id, botid, token) => {
+    var data = await Votecount(id, botid, token);
+    return(data)
 }
